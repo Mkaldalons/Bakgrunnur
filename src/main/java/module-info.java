3ebb6d12768da336c2ee74a1d411e0466d40ab9a
Maria Ros <mrk9@hi.is>
2023-04-05 14:29:10 +0000

    Bakgrunnur fyrir Main View

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module hi.style {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-                requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
     opens hi.style to javafx.fxml;
     exports hi.style;
 }
